d41634b05d60476523300240e63b4a5a6faf275f
lukeemhigh <lukeemhigh@archetype.localdomain>
2023-08-21 16:09:49 +0200

    moved checK_utils to lib path

---
+++ bash/lib/check-utils.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# Author: Luca Giugliardi
+# Email: luca.giugliardi@gmail.com
+#
+#####################################
+# Check if the given binaries are installed
+# Arguments:
+#   Utils. eg: aws, kubectl, eksctl
+#####################################
+check_utils() {
+	local utils=("$@")
+	for util in "${utils[@]}"; do
+		if ! which "$util" >/dev/null 2>&1; then
+			print_color "red" "[ERROR] $(date '+%Y-%m-%d %H:%M:%S') Cannot find ${util}, please make sure it's installed before running the script"
+			exit 1
+		fi
+	done
+}
