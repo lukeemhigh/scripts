85c211525cddd2b156ff9fdb8db399518babcb20
lukeemhigh <luca.giugliardi@res-it.com>
2023-11-03 21:19:36 +0100

    using proper env variable

--- bash/lib/conda-activate.sh
+++ bash/lib/conda-activate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 conda_activate() {
-	local venv="$1"
-	source /home/lukeemhigh/anaconda3/bin/activate "${venv}"
+    local venv="$1"
+    source "${HOME}"/anaconda3/bin/activate "${venv}"
 }
