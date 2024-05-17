b4aa08c858e1c6d2bbd51f7f46bd46ac5769fec0
lukeemhigh <lukeemhigh@archetype.localdomain>
2024-05-17 16:22:11 +0200

    chore[check-utils]: using log instead of print_color

--- bash/lib/check-utils.sh
+++ bash/lib/check-utils.sh
@@ -13,7 +13,7 @@ check_utils() {
   local utils=("$@")
   for util in "${utils[@]}"; do
     if ! which "$util" >/dev/null 2>&1; then
-      print_color "red" "[ERROR] $(date '+%Y-%m-%d %H:%M:%S') Cannot find ${util}, please make sure it's installed before running the script"
+      log error "Cannot find ${util}, please make sure it's installed before running the script"
       exit 1
     fi
   done
